75e5e9d35142d8e0f3fd34a718a674895dac649b
Nina Månsson <ninamansster@gmail.com>
2020-03-06 11:37:02 +0100

    fetch on cardID

--- server.js
+++ server.js
@@ -1,9 +1,73 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
-import cards from './data/cards.json'
+import mongoose from 'mongoose'
+import cardsData from './data/cards.json'
 
 
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/blue"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
+const Card = mongoose.model('Card', {
+  cardID: {
+    type: Number
+  },
+
+  title: {
+    type: String
+  },
+
+  image: {
+    type: String
+  },
+
+  fact: {
+    type: String
+  },
+
+  info_link: {
+    type: String
+  },
+
+  book_tip: {
+    type: String
+  },
+
+  language_code: {
+    type: String
+  }
+})
+
+//When the server runs with RESET_DATABASE=true npm run dev -> it will reset the database, reload MongoDB compass.
+//When the server runs with npm run dev, no updates are accepted to the server. 
+
+if (process.env.RESET_DATABASE) {
+  console.log('Resetting database!')
+
+  const seedDatabase = async () => {
+    await Card.deleteMany()
+
+
+    const addCardsToDatabase = () => {
+      cardsData.forEach((card) => {
+        new Card(card).save()
+      })
+    }
+    addCardsToDatabase()
+    //This adds a new collection of cards.
+  }
+  seedDatabase()
+}
+
+// {
+//   "cardID": 8,
+//   "title": "Pick up plastic garbage at the beach",
+//   "fact": "",
+//   "info_link": "",
+//   "book_tip": "",
+//   "language_code": "eng"
+// }
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
@@ -20,8 +84,32 @@ app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
-app.get('/cards', (req, res) => {
+// app.get('/cards', (req, res) => {
+//   res.json(cardsData)
+// })
+
+app.get('/cards', async (req, res) => {
+  const cards = await Card.find()
   res.json(cards)
+})
+
+// app.get('/cards:cardID', async (req, res) => {
+//   const cardID = req.params.cardID
+//  Card.findOne({ 'cardID': cardID })
+//   if (findThatCard) {
+//     res.json(findThatCard)
+//   } else {
+//     res.status(404).send('There was no card with that id')
+//   }
+// })
+app.get('/cards/:cardID', (req, res) => {
+  const cardID = req.params.cardID
+  Card.findOne({ 'cardID': +cardID })
+    .then((results) => {
+      res.json(results)
+    }).catch((err) => {
+      res.json({ message: 'Cannot find this card', err: err })
+    })
 })
 
 // Start the server
